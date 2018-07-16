57200f42a9c2b7a3e071eefeda5fef7f10941a42
DennisDaddy <softteez95@gmail.com>
2018-07-16 12:07:37 +0300

    Add user interface template

---
+++ UI/js/login.js
@@ -0,0 +1,16 @@
+function validate(){
+	var username = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
+	if(username == null||username==""){
+		alert("Please enter the username!");
+		return false;
+	}
+
+	if(password==null||password=="") {
+		alert("Please enter the password");
+		return false;
+	}
+
+	alert("Login successful")
+	window.location = "index.html";
+}
