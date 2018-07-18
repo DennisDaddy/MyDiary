e60f93ee570d32604f1f50d3bddef95e5ab6c15d
DennisDaddy <softteez95@gmail.com>
2018-07-18 13:56:56 +0300

    [feature #159072153] Enable entry creating

---
+++ API/app.py
@@ -0,0 +1,25 @@
+"""Import python modules"""
+from flask import Flask, jsonify, request, abort
+
+app = Flask(__name__)
+
+entries = [{'id' : 1, 'title' : u'this is one', 'content' : u'this is content'},
+           {'id' : 1, 'title' : u'this is one', 'content' : u'this is content'}]
+
+
+@app.route('/diary/api/v1/entries', methods=['POST'])
+def add_entry():
+    """Function for creating entry"""
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    entry = {
+        'id': entries[-1]['id'] + 1,
+        'title': request.json['title'],
+        'content': request.json['content']
+    }
+    entries.append(entry)
+    return jsonify({'entry': entry})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
