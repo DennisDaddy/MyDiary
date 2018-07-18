aec8bd6be029e4c9da4a8a4063ac8a34336626fa
DennisDaddy <softteez95@gmail.com>
2018-07-18 15:35:21 +0300

    [feature #159072215] View diary entry contents

--- API/app.py
+++ API/app.py
@@ -1,14 +1,14 @@
 """Import python modules"""
-from flask import Flask, jsonify , request,  abort
+from flask import Flask, jsonify, request, abort
 
 app = Flask(__name__)
 
 entries = [{'id' : 1, 'title' : u'this is one', 'content' : u'this is content'},
-            {'id' : 1, 'title' : u'this is two', 'content' : u'this is content'}]
+           {'id' : 1, 'title' : u'this is two', 'content' : u'this is content'}]
 
 @app.route('/diary/api/v1/entries', methods=['POST'])
 
-def add_comment():
+def create_entry():
     """ This is a funtion for creating an entry"""
     if not request.json or not 'title' in request.json:
         abort(400)
@@ -20,9 +20,19 @@ def add_comment():
     entries.append(entry)
     return jsonify({'entry' : entry})
 
-@app.route('/diary/api/entries', methods=['GET'])
+@app.route('/diary/api/v1/entries', methods=['GET'])
 def get_entries():
+    """This is a function for viewing a list of diary entries"""
     return jsonify({'entries' : entries})
+
+@app.route('/diary/api/v1/entries/<int:entry_id>', methods=['GET'])
+def view_one_entry(entry_id):
+    """This is a function for getting a single entry"""
+    entry = [entry for entry in entries if entry['id'] == entry_id]
+    if len(entry) == 0:
+        abort(404)
+    return jsonify({'entry': entry[0]})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
