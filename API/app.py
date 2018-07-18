5ad9e5a3d0b7b29c57abdfc305ca27a0e1810ba3
DennisDaddy <softteez95@gmail.com>
2018-07-18 17:21:18 +0300

    [feature #159140668] Enable user registration

--- API/app.py
+++ API/app.py
@@ -5,6 +5,29 @@ app = Flask(__name__)
 
 entries = [{'id' : 1, 'title' : u'this is one', 'content' : u'this is content'},
            {'id' : 1, 'title' : u'this is two', 'content' : u'this is content'}]
+user_info = {}
+
+@app.route('/diary/api/v1/register', methods=['POST'])
+def register():
+    """This is a function for registering a user"""
+    username = request.get_json()["username"]
+    email = request.get_json()["email"]
+    password = request.get_json()["password"]
+    password_confirmation = request.get_json()["password_confirmation"]
+
+    if not username or username == 0:
+        return jsonify({"message": "Username cannot be blank"}), 401
+    elif not email:
+        return jsonify({"message": "Email cannot be blank"}), 401
+
+    elif not password:
+        return jsonify({"message": "Passord cannot be blank"}), 401
+    user_info.update({username:{"email": email, "password": password,
+                      "password_confirmation" : password_confirmation}})
+    if username in user_info:
+        return jsonify({'message': "You are successfully registered"})
+    return jsonify({'message': "You are successfully registered"})
+
 
 @app.route('/diary/api/v1/entries', methods=['POST'])
 
@@ -29,14 +52,15 @@ def get_entries():
 def view_one_entry(entry_id):
     """This is a function for getting a single entry"""
     entry = [entry for entry in entries if entry['id'] == entry_id]
-    if len(entry) == 0:
+    if entry == 0:
         abort(404)
     return jsonify({'entry': entry[0]})
 
 @app.route('/diary/api/v1/entries/<int:entry_id>', methods=['PUT'])
 def modify_entry(entry_id):
+    """This is a function for modifying an entry"""
     entry = [entry for entry in entries if entry['id'] == entry_id]
-    if not request.json:
+    if entry == 0:
         abort(404)
     entry[0]['title'] = request.json.get('title', entry[0]['title'])
     entry[0]['content'] = request.json.get('content', entry[0]['content'])
@@ -46,7 +70,7 @@ def modify_entry(entry_id):
 def delete_entry(entry_id):
     """This is a function for deleting an entry"""
     entry = [entry for entry in entries if entry['id'] == entry_id]
-    if len(entry) == 0:
+    if entry == 0:
         return jsonify({'message' : "No entry found"})
     entries.remove(entry[0])
     return jsonify({'result': True})
