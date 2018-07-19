f9e7885183840a348b5d0f1eea6ab87f12da216c
DennisDaddy <softteez95@gmail.com>
2018-07-19 08:47:23 +0300

    [feature #159075320] Test an entry

--- tests.py
+++ tests.py
@@ -4,10 +4,11 @@ from app import *
 import json
 
 class EntryTestCase(unittest.TestCase):
-    def test_home(self):
+    def test_entry(self):
         tester = app.test_client(self)
-        response = tester.get('/')
+        response = tester.get('/diary/api/v1/entries/1', content_type='application/json')
         self.assertEqual(response.status_code, 200)
+        json_data = json.loads(response.data)
     
 
 
