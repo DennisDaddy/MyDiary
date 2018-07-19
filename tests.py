5dd53e4757a070b0347bc33b61e5073ec0f02908
DennisDaddy <softteez95@gmail.com>
2018-07-19 18:41:28 +0300

    Fix code climate method cognivitive

--- tests.py
+++ tests.py
@@ -11,12 +11,14 @@ class EntryTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Welcome to mydiary', response.data)
 
-    def test_view_entry(self):
-        ls = app.test_client(self)
-        response = ls.get('/diary/api/v1/entries/1', content_type='application/json')
+    def test_register(self):
+        ts = app.test_client(self)
+        response = ts.post('/diary/api/v1/auth/register', json={'username': 'dennis','email': 'de@gmail.com',
+         'password': 'secret', 'password_confirmation': 'secret'})
+        json_data = response.get_json()
         self.assertEqual(response.status_code, 200)
-        json_data = json.loads(response.data)
-    
+
+   
     def test_create_entry(self):
 		tester = app.test_client(self)
 		response = tester.post('/diary/api/v1/entries',
@@ -24,6 +26,13 @@ class EntryTestCase(unittest.TestCase):
 			follow_redirects =True
 		)
 		self.assertEqual(response.status_code, 200)
+        
+    def test_delete_entry(self):
+        tester = app.test_client(self)
+        response = tester.delete('/diary/api/v1/entries/1', content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
+
     def test_view_entries(self):
         tester = app.test_client(self)
         response = tester.get('/diary/api/v1/entries', content_type='application/json')
@@ -34,10 +43,14 @@ class EntryTestCase(unittest.TestCase):
         response = rm.get('/diary/api/v1/entries/1', content_type='application/json')
         self.assertEqual(response.status_code, 200)
 
-    def test_delete_entry(self):
-        tester = app.test_client(self)
-        response = tester.delete('/diary/api/v1/entries/1', content_type='application/json')
+    def test_view_entry(self):
+        ls = app.test_client(self)
+        response = ls.get('/diary/api/v1/entries/1', content_type='application/json')
         self.assertEqual(response.status_code, 200)
+        json_data = json.loads(response.data)
+    
+
+   
     
     def test_login(self):
         vm = app.test_client(self)
@@ -52,12 +65,7 @@ class EntryTestCase(unittest.TestCase):
         self.assertIn(b'you are successfully logged out', response.data)
 
     
-    def test_register(self):
-        ts = app.test_client(self)
-        response = ts.post('/diary/api/v1/auth/register', json={'username': 'dennis','email': 'de@gmail.com',
-         'password': 'secret', 'password_confirmation': 'secret'})
-        json_data = response.get_json()
-        self.assertEqual(response.status_code, 200)
+    
     
     def test_user_info(self):
         tester = app.test_client(self)
