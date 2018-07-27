1dfcd11e1b2d4f2d81952304e6e76335337193df
DennisDaddy <softteez95@gmail.com>
2018-07-27 13:20:05 +0300

    [Chore] Add user tests

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,38 +1,50 @@
 """Import python modules"""
 import unittest
-from app import app
+import json
+import os
+import sys
+from app import *
+
+sys.path.insert(0, os.path.abspath(".."))
 
 
 class MyDiaryTestCase(unittest.TestCase):
     """This test case represents test for user and diary entry testcase"""
+    def setUp(self):
+        """Define test variables and initialize app."""
+        self.app = create_app(config_name="testing")
+        self.client = self.app.test_client
 
     def test_register(self):
         """"Test user registration"""
         with app.test_client(self) as toaster:
-            response = toaster.post('/diary/api/v1/auth/register',
+            response = toaster.post('/api/v1/auth/register',
+                                    json={'username': 'dennis', 'email': 'de@gmail.com',
+                                          'password': 'secret', 'password_confirmation': 'secret'})
+            self.assertEqual(response.status_code, 200)
+
+    def test_wrong_registration(self):
+        """"Test user registration"""
+        with app.test_client(self) as toaster:
+            response = toaster.post('/api/v1/auth/register',
                                     json={'username': 'dennis', 'email': 'de@gmail.com',
                                           'password': 'secret', 'password_confirmation': 'secret'})
             self.assertEqual(response.status_code, 200)
 
 
+
     def test_login(self):
         """"Test logging in"""
         tester1 = app.test_client(self)
-        response = tester1.post('/diary/api/v1/auth/login',
+        response = tester1.post('/api/v1/auth/login',
                                 json={'username' : 'dennis', 'password': 'secret'})
 
         self.assertEqual(response.status_code, 200)
 
-    def test_logout(self):
-        """"Test logging out"""
-        with app.test_client(self) as tester:
-            response = tester.delete('/logout', content_type='application/json')
-            self.assertEqual(response.status_code, 200)
-            self.assertIn(b'you are successfully logged out', response.data)
+   
     def test_user_info(self):
-        """"Test user account information"""
-        tester = app.test_client(self)
-        response = tester.get('/diary/api/v1/users/1')
+        """"Test user account information"""        
+        response = self.client().get('/api/v1/users/1')
         self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
